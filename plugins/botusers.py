d57c931c3643f0b54601b19a41e697d5db768bf8
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-12 00:08:41 +0530

    Update and rename lazyusers.py to botusers.py

--- plugins/botusers.py
+++ plugins/botusers.py
@@ -9,7 +9,7 @@ from helper.database import botdata, find_one, total_user,getid
 
 from helper.progress import humanbytes
 
-@Client.on_message(filters.private & filters.user(ADMIN)  & filters.command(["lazyusers"]))
+@Client.on_message(filters.private & filters.user(ADMIN)  & filters.command(["botusers"]))
 async def start(client,message):
 	botdata(int(botid))
 	data = find_one(int(botid))
@@ -19,5 +19,5 @@ async def start(client,message):
 	ids = id.split(',')
 
 	await message.reply_text(f"⚡️ All IDS : {ids}\n\n⚡️ Total User :- {total_user()}\n\n⚡️ Total Renamed File :- {total_rename}\nV Total Size Renamed :- {humanbytes(int(total_size))}",quote=True,
-                             reply_markup= InlineKeyboardMarkup([[InlineKeyboardButton("🦋 Close Menu 🦋", callback_data="cancel")]]) 
+                             reply_markup= InlineKeyboardMarkup([[InlineKeyboardButton("Close Menu", callback_data="cancel")]]) 
                              )
