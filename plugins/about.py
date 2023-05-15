572f310d4b13affe71be0323860af581f576b8a9
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-16 01:36:41 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','5217294686')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 
